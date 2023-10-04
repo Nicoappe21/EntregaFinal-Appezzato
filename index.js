bdb3d79d9e967990f4b29502984407d34c0e73d8
Nicoappe21 <nicoappe@hotmail.com>
2023-10-03 21:50:19 -0300

    Entrega Final

--- index.js
+++ index.js
@@ -5,81 +5,28 @@ class Jugador{
         this.equipo = equipo
     }
 }
-const jugadores= [
-    {
-        nombre: `L.Messi`,
-        posicion: `Delantero`,
-        equipo: `Inter Miami`,
-        elegido: false
-    },
-    {
-        nombre: `J.Alvarez`,
-        posicion: `Delantero`,
-        equipo: `Manchester City`,
-        elegido: false
-    },
-    {
-        nombre: `E.Fernandez`,
-        posicion: `Mediocampista`,
-        equipo: `Chelsea`,
-        elegido: false
-    },
-    {
-        nombre: `R.De Paul`,
-        posicion: `Mediocampista`,
-        equipo: `Atletico Madrid`,
-        elegido: false
-    },
-    {
-        nombre: `A.MacAllister`,
-        posicion: `Mediocampista`,
-        equipo: `Liverpool`,
-        elegido: false
-    },
-    {
-        nombre: `C.Romero`,
-        posicion: `Defensor`,
-        equipo: `Tottenham`,
-        elegido: false
-    },
-    {
-        nombre: `E.Martinez`,
-        posicion: `Arquero`,
-        equipo: `Aston Villa`,
-        elegido: false
-    },
-    {
-        nombre: `N.Tagliafico`,
-        posicion: `Defensor`,
-        equipo: `O.Marsella`,
-        elegido: false
-    },
-    {
-        nombre: `N.Molina`,
-        posicion: `Defensor`,
-        equipo: `Atletico Madrid`,
-        elegido: false
-    },
-    {
-        nombre: `A.Di Maria`,
-        posicion: `Delantero`,
-        equipo: `Benfica`,
-        elegido: false
-    },
-]
+
+let jugadores = []
+fetch(`plantel.json`)
+    .then(res => res.json())
+    .then(data =>{
+        jugadores = data
+    })
+
+
 let jugadoresElegidos= []
 let jugadoresRivales= []
 
 
 
-let sectionBienvenido = document.getElementById("bienvenido")
+const sectionBienvenido = document.getElementById("bienvenido")
 let sectionJugadores = document.getElementById("jugadores")
 let sectionBotones = document.getElementById("botones-lista")
 const botonComenzar = document.getElementById("comenzar")
 const usuarioInput = document.getElementById("usuario")
 const divSectionJugadores= document.getElementById("div-jugadores")
-
-
+let nombreUsuario = document.getElementById("titulo-jugadores");
+const seccionBotones = document.querySelector(".botones-lista");
 
 
 function paginaInicial() {
@@ -90,16 +37,72 @@ function paginaInicial() {
         jugadoresElegidos.forEach((jugador) => {
             const idx = jugadores.findIndex((item) => item.nombre === jugador.nombre);
             if (idx !== -1) {
-                const botonConvocar = document.querySelectorAll(".convocar")[idx];
+                const botonConvocar = document.getElementsByClassName("convocar");
                 botonConvocar.disabled = true;
             }
         });
     }
 }
 
+function iniciarJuego() {
+
+    let usuario = usuarioInput.value;
+    if (usuario === "") {
+        alert("Escribe un nombre para comenzar");
+    } else {
+        sectionBienvenido.style.display = "none";
+        
+        nombreUsuario.innerText = ` Bienvenido ${usuario}, ahora eres el DT, elige 5 jugadores para patear los penales`;
+
+        jugadores.forEach(function (jugador, index) {
+            const cartaJugador = document.createElement("div");
+            cartaJugador.className = "carta-jugador";
+
+            cartaJugador.innerHTML = `
+            <h2>${jugador.nombre}</h2>
+            <h3>${jugador.posicion}</h3>
+            <h3>${jugador.equipo}</h3>
+            <button class="convocar">Convocar</button>
+            `;
+
+            const botonConvocar = cartaJugador.querySelector("button");
+            if (jugadoresElegidos.some((elegido) => elegido.nombre === jugador.nombre)) {
+                botonConvocar.disabled = true;
+            }
+            if (jugador.elegido) {
+                botonConvocar.disabled = true;
+            } else {
+                botonConvocar.addEventListener("click", function () {
+                    convocarJugador(index, botonConvocar);
+                });
+            }
+            divSectionJugadores.appendChild(cartaJugador);
+        });
+
+        const botonVerEquipo = document.createElement("button");
+        botonVerEquipo.textContent = "Ver Equipo";
+        botonVerEquipo.id = "VerEquipo";
+
+        const botonConfirmarEquipo = document.createElement("button");
+        botonConfirmarEquipo.textContent = "Confirmar Equipo";
+        botonConfirmarEquipo.id = "confirmar";
+
+        const botonVolverAEmpezar = document.createElement("button");
+        botonVolverAEmpezar.textContent = "Volver a empezar";
+        botonVolverAEmpezar.id = "reset";
+
+        seccionBotones.appendChild(botonVerEquipo);
+        seccionBotones.appendChild(botonConfirmarEquipo);
+        seccionBotones.appendChild(botonVolverAEmpezar);
+        botonVolverAEmpezar.addEventListener("click", resetear);
+        botonVerEquipo.addEventListener("click", mostrarEquipo);
+        botonConfirmarEquipo.addEventListener("click", confirmarEquipo);
+    }
+}
+
 function chequearEquipo(){
     let botonConvocar = document.getElementsByClassName("convocar")
-    jugadoresElegidos.length < 4 ? botonConvocar.disabled : botonConvocar.disabled = false 
+    jugadoresElegidos.length >= 5 ? botonConvocar.disabled : botonConvocar.disabled = false 
 }
 
 function convocarJugador(idx, botonConvocar) {
@@ -152,79 +155,39 @@ function mostrarEquipo() {
 }
 }
 function confirmarEquipo(){
+    
+    
     if (jugadoresElegidos.length < 5){
         alert("Elige 5 jugadores para tu equipo")
     } else {
+    divSectionJugadores.style.display = "none"
+    nombreUsuario.style.display = "none"
+    seccionBotones.style.display= "none"
     const equipoInfo = document.getElementById("equipo-info");
-    equipoInfo.innerHTML = "ESTE ES TU EQUIPO";
+    equipoInfo.innerHTML = "Es el momento de la verdad, elige donde patear cada penal";
     const listaJugadores = document.createElement("ul");
     jugadoresElegidos.forEach(function (jugador) {
         const jugadorItem = document.createElement("li");
-        jugadorItem.textContent = `${jugador.nombre} (${jugador.posicion}) - ${jugador.equipo}`;
+        jugadorItem.textContent = `${jugador.nombre}`;
+        const botonIzquierda = document.createElement("button")
+        botonIzquierda.className = "boton-patear"
+        botonIzquierda.innerText = "Izquierda"
+        jugadorItem.appendChild(botonIzquierda)
+        const botonMedio = document.createElement("button")
+        botonMedio.className = "boton-patear"
+        botonMedio.innerText = "Medio"
+        jugadorItem.appendChild(botonMedio)
+        const botonDerecha = document.createElement("button") 
+        botonDerecha.className = "boton-patear"
+        botonDerecha.innerText = "Derecha"
+        jugadorItem.appendChild(botonDerecha)
         listaJugadores.appendChild(jugadorItem);
     });
     equipoInfo.appendChild(listaJugadores);
     alert("Equipo confirmado")
 } 
 }
-function iniciarJuego() {
-    let usuario = usuarioInput.value;
-    if (usuario === "") {
-        alert("Escribe un nombre para comenzar");
-    } else {
-        sectionBienvenido.style.display = "none";
-        let nombreUsuario = document.getElementById("titulo-jugadores");
-        nombreUsuario.innerText = ` Bienvenido ${usuario}, ahora eres el DT, elige a tus 5 jugadores`;
 
-        jugadores.forEach(function (jugador, index) {
-            const cartaJugador = document.createElement("div");
-            cartaJugador.className = "carta-jugador";
-
-            cartaJugador.innerHTML = `
-            <h2>${jugador.nombre}</h2>
-            <h3>${jugador.posicion}</h3>
-            <h3>${jugador.equipo}</h3>
-            <button class="convocar">Convocar</button>
-            `;
-
-            const botonConvocar = cartaJugador.querySelector("button");
-
-            if (jugadoresElegidos.some((elegido) => elegido.nombre === jugador.nombre)) {
-                botonConvocar.disabled = true;
-            }
-
-            if (jugador.elegido) {
-                botonConvocar.disabled = true;
-            } else {
-                botonConvocar.addEventListener("click", function () {
-                    convocarJugador(index, botonConvocar);
-                });
-            }
-
-            divSectionJugadores.appendChild(cartaJugador);
-        });
-
-        const seccionBotones = document.querySelector(".botones-lista");
-
-        const botonVerEquipo = document.createElement("button");
-        botonVerEquipo.textContent = "Ver Equipo";
-        botonVerEquipo.id = "VerEquipo";
-
-        const botonConfirmarEquipo = document.createElement("button");
-        botonConfirmarEquipo.textContent = "Confirmar Equipo";
-        botonConfirmarEquipo.id = "confirmar";
-
-        const botonVolverAEmpezar = document.createElement("button");
-        botonVolverAEmpezar.textContent = "Volver a empezar";
-        botonVolverAEmpezar.id = "reset";
-
-        seccionBotones.appendChild(botonVerEquipo);
-        seccionBotones.appendChild(botonConfirmarEquipo);
-        seccionBotones.appendChild(botonVolverAEmpezar);
-        botonVolverAEmpezar.addEventListener("click", resetear);
-        botonVerEquipo.addEventListener("click", mostrarEquipo);
-        botonConfirmarEquipo.addEventListener("click", confirmarEquipo);
-    }
-}
 
 paginaInicial()
+git status
